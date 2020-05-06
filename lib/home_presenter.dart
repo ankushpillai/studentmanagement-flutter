0e00a32bddc0602cf14d3922a03bd3ed1c6a313f
Ankush R Pillai <provider@softechworld.com>
2020-05-06 12:17:18 +0530

    Initial commit

---
+++ lib/home_presenter.dart
@@ -0,0 +1,27 @@
+import 'package:studentapp/database/database_helper.dart';
+import 'package:studentapp/database/model/user.dart';
+import 'dart:async';
+
+abstract class HomeContract {
+  void screenUpdate();
+}
+
+class HomePresenter {
+  HomeContract _view;
+  var db = new DatabaseHelper();
+  HomePresenter(this._view);
+  delete(User user) {
+    var db = new DatabaseHelper();
+    db.deleteUsers(user);
+    updateScreen();
+  }
+
+  Future<List<User>> getUser() {
+    return db.getUser();
+  }
+
+  updateScreen() {
+    _view.screenUpdate();
+
+  }
+}
